05b929976fbfe08f78b7c567e2041f5d30974712
Musthafa Abdul Rahiman <musthafaabdulrahiman@Musthafas-MacBook-Pro.local>
2026-09-14 16:47:58 +0400

    chore: remove unnecessary documentation and test scripts

--- test-cover.ts
+++
@@ -1,2 +0,0 @@
-import { getBookCoverUrl } from './lib/openLibrary';
-getBookCoverUrl("The Vignelli Canon Massimo Vignelli").then(console.log);
--- test-imports.js
+++
@@ -1,3 +0,0 @@
-import * as THREE from 'three';
-import { Canvas, useFrame } from '@react-three/fiber';
-console.log(!!Canvas);
